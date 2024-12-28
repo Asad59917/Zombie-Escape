fefaeea0ec673b0e108c40b1cd6c1436e9be3f8f
Asad59917 <144930871+Asad59917@users.noreply.github.com>
2024-12-28 22:04:43 +0400

    Player dying animaton

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -15,18 +15,27 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 MovementInput;
     private Vector2 SmoothMovementIutput;
     private Vector2 MovementIutputSmoothVelocity;
+    private Animator animator;
 
     private void Awake()
     {
        RigiBody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         SetPlayerVelocity();
         RotateDirectionInput();
+        setAnimation();
     }
 
+    private void setAnimation()
+    {
+        bool ismoving = MovementInput != Vector2.zero;
+
+        animator.SetBool("IsMoving", ismoving);
+    }
     private void SetPlayerVelocity()
     {
         SmoothMovementIutput = Vector2.SmoothDamp(
